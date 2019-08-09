6c1fcd180335f0281ce0f57e55d4965e600df8da
Bohdan Hrytsenko <clickatell.bh@gmail.com>
2019-08-09 15:03:10 -0700

    Detect Pangram

--- src/codewars/DetectPangram.java
+++ src/codewars/DetectPangram.java
@@ -1,16 +1,41 @@
 package codewars;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DetectPangram {
 
     public static void main(String[] args) {
-        System.out.println(check("The quick brown fox jumps over the lazy dog."));
+        System.out.println(check("The quick brown fox jumps over the lazy dog.")); //true
+        System.out.println(check("The quick brown ")); //false
+        System.out.println(check("rwtg yh mnold jfpiq acs")); //false
+        System.out.println(check("3 3 4 33")); //true
+        System.out.println(check("3")); //true
     }
 
+                //Detect Pangram
+        //A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog"
+        //is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+        //Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 
-
+                //#1
     public static boolean check (String sentence) {
+        sentence = sentence.replaceAll("[^a-zA-Z]", "");
+        char [] str = sentence.toLowerCase().toCharArray();
+        Map <Character, String> map = new HashMap<>();
 
-        return false;
+        if (str.length == 0) return true;
+        if (str.length < 26) return false;
+        for (Character x : str) map.put(x, "");
+        return true;
+
+    }
+                //#2
+    public boolean check2(String sentence){
+        for (char c = 'a'; c<='z'; c++)
+            if (!sentence.toLowerCase().contains("" + c))
+                return false;
+        return true;
 
     }
 }
